83f79ed565416ccdb6ff8f6b34e0bc72059e5796
evansshaun515 <76917386+evansshaun515@users.noreply.github.com>
2021-02-16 13:37:39 -0600

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -7,7 +7,8 @@ class dustbin
 		this.dustbinWidth=200;
 		this.dustbinHeight=213;
 		this.wallThickness=20;
-		
+		this.image = loadImage('dustbingreen.png');
+
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 		
--- sketch.js
+++ sketch.js
@@ -6,18 +6,19 @@ const Body = Matter.Body;
 const Render = Matter.Render;
 var dustbinObj,groundObject	
 var world;
-
+var paper
 
 function setup() {
-	createCanvas(1600, 700);
+	createCanvas(1250,605);
 	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
 	
-	groundObject=new ground(width/2,670,width,20);
-	dustbinObj=new dustbin(1200,650);
+	groundObject=new ground(width/2,575,width,20);
+	dustbinObj=new dustbin(1050,555);
+	paper = new Paper(200,450,60,60);
 
 	Engine.run(engine);
   
@@ -31,6 +32,7 @@ function draw() {
 
   groundObject.display();
   dustbinObj.display();
+  paper.display();
 
 }
 
